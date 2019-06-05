7509f5c2dbb18fd4d2bae34ae5c8473226823d63
Florian Gruber <florian.dominik.gruber@student.uni-augsburg.de>
2019-06-05 12:20:09 +0200

    übung6 initial commit

---
+++ übung6/CNN.py
@@ -0,0 +1,167 @@
+import numpy as np
+import gzip
+import scipy.io as sio
+import pickle
+import time
+import random
+
+
+def nanargmx(a):
+    idx = np.argmax(a, axis=None)
+    multi_idx = np.unravel_index(idx, a.shape)
+    if np.isnan(a[multi_idx]):
+        nan_count = np.sum(np.isnan(a))
+        idx = np.argpartition(a, -nan_count-1, axis=None)[-nan_count-1]
+        multi_idx = np.unravel_index(idx, a.shape)
+    return multi_idx
+
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 0.0)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
+    return vector
+
+
+def conv(image, f, bias):
+
+    padding = 0
+    stride = 1
+
+    if padding > 0:
+        img = np.zeros(
+            (image.shape[0], image.shape[1]+padding*2, image.shape[2]+padding*2))
+        for dim in range(image.shape[0]):
+            img[dim] = np.pad(image[dim], padding, pad_with)
+        image = img
+
+    D, H, W = image.shape
+    d, h, w = f.shape
+
+    h_out = (H - h + 2 * padding) / stride + 1
+    w_out = (W - w + 2 * padding) / stride + 1
+
+    feature_map = np.zeros((D, int(h_out), int(w_out)))
+
+    S = W
+    s = w
+
+    for dim in range(D):
+        i = 0
+        while i < S-1:
+            j = 0
+            while j < S-1:
+                window = image[dim, j:s+j, i:s+i]
+                f_map = np.dot(window, f[0])
+                f_map = np.dot(f_map, bias)
+                f_1 = np.sum(f_map)
+                feature_map[dim, int(i/stride), int(j/stride)] = f_1
+                j += stride
+                pass
+
+            i += stride
+            pass
+
+    return feature_map
+
+
+def relu(feature_map):
+    relu_out = np.zeros(feature_map.shape)
+    for map_num in range(feature_map.shape[-1]):
+        for r in np.arange(0, feature_map.shape[0]):
+            for c in np.arange(0, feature_map.shape[1]):
+                relu_out[r, c, map_num] = np.max(
+                    [feature_map[r, c, map_num], 0])
+    return relu_out
+
+
+def max_pooling(image, f, strd):
+    D, H, W = image.shape
+    filter_size = f
+
+    stride = strd
+
+    h_out = (H - f) / stride + 1
+    w_out = (W - f) / stride + 1
+
+    pool = np.zeros((D, int(h_out), int(w_out)))
+
+    S = W
+
+    for dim in range(D):
+        i = 0
+        while i < S-1:
+            j = 0
+            while j < S-1:
+                pooling = image[dim, j:filter_size+j, i:filter_size+i]
+                p_1 = np.max(pooling)
+                pool[dim, int(i/stride), int(j/stride)] = p_1
+                j += stride
+                pass
+
+            i += stride
+            pass
+    return pool
+
+
+def softmax(feature_map):
+    softmax_out = np.zeros(feature_map.shape)
+
+    for dim in range(feature_map.shape[0]):
+        x = feature_map[dim]
+        e_x = np.exp(x - np.max(x))
+        res = e_x / e_x.sum()
+        softmax_out[dim] = res
+
+    return softmax_out
+
+
+class FFNN(object):
+    def __init__(self, weights):
+        self.inputSize = 2
+        self.outputSize = 2
+        self.hiddenSize = 1
+        self.w1 = weights
+        # self.w2 = np.random.randn(self.hiddenSize,self.outputSize)
+        d, h, w = weights.shape
+        self.w2 = np.random.randn(d, h, w)
+
+    def forward(self, X):
+        # dot product of input and first set of weights
+        self.z = np.zeros(X.shape)
+        self.z2 = np.zeros(X.shape)
+
+        for dim in range(X.shape[0]):
+            self.z[dim] = np.dot(X[dim], self.w1[dim])
+            # activation function
+            self.z2[dim] = softmax(self.z[dim])
+            pass
+
+        o = self.z2
+        return o
+
+    def softmaxDerivative(self, x):
+        return x * (1 - x)
+
+    def backward(self, X, y, o):
+        # backward propgate through the network
+        self.o_error = y - o
+        self.o_delta = self.o_error * self.softmaxDerivative(o)
+        self.z2_error = np.zeros(X.shape)
+        self.z2_delta = np.zeros(X.shape)
+
+        for dim in range(X.shape[0]):
+            self.z2_error[dim] = self.o_delta[dim].dot(self.w2[dim].T)
+            self.z2_delta[dim] = self.z2_error[dim] * self.softmaxDerivative(self.z2[dim])
+            # adjusting weights
+            self.w1[dim] += X[dim].T.dot(self.z2_delta[dim])
+            self.w2[dim] += self.z2[dim].T.dot(self.o_delta[dim])
+            pass
+
+    def train(self, X, y):
+        o = self.forward(X)
+        self.backward(X, y, o)
+
+    def predict(self):
+        print("Predicted data based on trained weights: ")
+        print( "Output: \n")
---
+++ übung6/main.py
@@ -0,0 +1,66 @@
+import CNN
+import numpy as np
+
+X = np.random.rand(3, 12, 12)
+
+l1_filter = np.random.rand(1, 2, 2)
+bias1 = 1
+
+l2_filter = np.random.rand(1, 2, 2)
+bias2 = 1
+
+# theta3 = l1_filter
+theta3 = np.random.rand(3, 2, 2)
+bias3 = 1
+
+from sklearn.metrics import log_loss
+
+def cross_entropy(predictions, targets, epsilon=1e-12):
+    predictions = np.clip(predictions, epsilon, 1. - epsilon)
+    N = predictions.shape[0]
+    ce = -np.sum(targets*np.log(predictions+1e-9))/N
+    return ce
+
+def TwoLayerCNN(image, filt1, filt2, bias1, bias2, theta3, bias3, epochs=300):
+    feature_map = CNN.conv(image, filt1, bias1)
+    feature_map_relu = CNN.relu(feature_map)
+    max_pool = CNN.max_pooling(feature_map_relu, 2, 2)
+    feature_map2 = CNN.conv(max_pool, filt2, bias2)
+    feature_map_relu2 = CNN.relu(feature_map2)
+    max_pool2 = CNN.max_pooling(feature_map_relu2, 2, 2) 
+
+    DenseNetwork = CNN.FFNN(theta3)
+
+    pred_output = DenseNetwork.forward(max_pool2)
+    y = np.random.randint(10, size=pred_output.shape)
+    cel = cross_entropy(y, pred_output)
+
+    for i in range(epochs+1):
+        print('\nepoch {num}'.format(num = i))
+        print('\npredicted output:\n' + str(pred_output))
+        print('\nloss:\n' + str(cel))
+        print('\n---------------------------------------------')
+        DenseNetwork.train(max_pool2, y)
+
+    return pred_output, cel
+
+feature_map = CNN.conv(X, l1_filter, bias1)
+print('\n==========================\nFeature Map 1\n==========================\n', feature_map)
+
+feature_map_relu = CNN.relu(feature_map)
+print('\n==========================\nFeature Map ReLU 1\n==========================\n', feature_map_relu)
+
+max_pool = CNN.max_pooling(feature_map_relu, 2, 2)
+print('\n==========================\nMax Pooling 1\n==========================\n', max_pool)
+
+feature_map2 = CNN.conv(max_pool, l2_filter, bias2)
+print('\n==========================\nFeature Map 2\n==========================\n', feature_map2)
+
+feature_map_relu2 = CNN.relu(feature_map2)
+print('\n==========================\nFeature Map ReLU 2\n==========================\n', feature_map_relu2)
+
+max_pool2 = CNN.max_pooling(feature_map_relu2, 2, 2)
+print('\n==========================\nMax Pooling 2\n==========================\n', max_pool2)
+
+output = TwoLayerCNN(X, l1_filter, l2_filter, bias1, bias2, theta3, bias3)
+print('\n==========================\nFeed-Forward CNN\n\nCross-Entropy Loss = {loss}\n\nPredicted output:\n{out}\n==========================\n'.format(loss = output[1], out = output[0]))
