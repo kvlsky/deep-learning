42ddabe41899ab3e733e99829fb15a122074ad83
kovaland <andrii.koval@student.uni-augsburg.de>
2019-07-04 09:02:14 +0200

    Update ImgClf.py

--- classifier/ImgClf.py
+++ classifier/ImgClf.py
@@ -14,17 +14,17 @@ def ImgClf(input_shape, num_classes, epochs):
     model.add(
         Conv2D(32, (3, 3), activation="relu", input_shape=input_shape)
     )
+    model.add(Conv2D(32, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.1))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
     model.add(Conv2D(64, (3, 3), activation="relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.1))
 
-    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu"))
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.1))
-
-    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu"))
-    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation="relu"))
+    model.add(Conv2D(128, (3, 3), activation="relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.1))
 
